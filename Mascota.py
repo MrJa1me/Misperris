93275f440841eda7f8a3a31722025dd44e871997
MrJa1me <guajardoinfo@gmail.com>
2025-04-17 12:21:59 -0400

    main

---
+++ Mascota.py
@@ -0,0 +1,16 @@
+class Mascota:
+    chip=''
+    nombre=''
+    raza=''
+    genero=''
+    peso=0
+    edad=0
+    # constuctor de la clase
+    def __init__(self):
+        self.chip='0000000000'
+        self.nombre='NN'
+        self.raza='SR'
+        self.peso=0
+        self.edad=0
+        self.genero='SG'
+    
---
+++ main.py
@@ -0,0 +1,16 @@
+ #fundacion de adopcion de mascotas
+    #MIS PERRIS
+ 
+import Mascota
+
+#se crea un OBJETO  a partir de l clase
+m1 = Mascota
+#se modifican sus atributos
+m1.nombre='doki'
+m1.edad=10
+m1.raza='Terrier chileno'
+m1.genero='Macho'
+m1.chip='1010101'
+#mostrar atributos
+print(f"el nombre de la mascota es: {m1.nombre}")
+print(f"la raza de la mascota es: {m1.raza}")
